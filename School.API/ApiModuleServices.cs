88f9ad9d0f4e41d9b0c9bb63659a12f14528c3a7
Eissa Ahmed <essa1998ahmed@gmail.com>
2023-12-22 23:28:32 +0200

    00

--- School.API/ApiModuleServices.cs
+++ School.API/ApiModuleServices.cs
@@ -2,13 +2,25 @@
 
 public static class ApiModuleServices
 {
-    public static IServiceCollection ApplyApiModuleServices(this IServiceCollection services)
+    public static IServiceCollection ApplyApiModuleServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Add services to the container.
         services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+
+        //Logger
+        services.ApplyLogger(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection ApplyLogger(this IServiceCollection services, IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
 
         return services;
     }
--- School.API/GlobalUsing.cs
+++ School.API/GlobalUsing.cs
@@ -2,3 +2,5 @@
 global using School.Core;
 global using School.Infrastructure;
 global using School.Services;
+global using Serilog;
+
